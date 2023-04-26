ec7b55435708fa34ee1e35763745bcb53322dc91
Chris Daniels <danielschris96@gmail.com>
2023-04-25 21:32:52 -0700

    added get routes

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express');
-
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("./public"));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+const readFileData = util.promisify(fs.readFile);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFileData('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+  
+
+
+
 
 
 app.listen(PORT, () => {
